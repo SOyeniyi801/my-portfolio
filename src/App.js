ae31bc7d0c6550cf529f6f3bfa143decd12d1488
soyeniyi801 <naturalbeauty801@gmail.com>
2024-12-02 12:04:28 -0500

    fix usepagetracking

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React, { useEffect }from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css';
-// import usePageTracking from "./hooks/usePageTracking";
-import Navbar from './Pages/Home/Navbar';
-import Home from './Pages/Home/Homescreen';
+import "./App.css";
+import usePageTracking from "./hooks/usePageTracking";
+import Navbar from "./Pages/Home/Navbar";
+import Home from "./Pages/Home/Homescreen";
 
-
-function App (){
-  // usePageTracking();
-  useEffect(() => {
-    document.title = "Sammi Web Dev";
-  }, []);
+function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
+        {/* Now inside Router context, usePageTracking works properly */}
+        <PageTrackingWrapper />
         <div>
           <Navbar />
           <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='*' element={<div>404 Not Found</div>}></Route>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<div>404 Not Found</div>} />
           </Routes>
         </div>
       </Router>
-      
     </div>
   );
 }
+
+// This ensures proper use of usePageTracking
+const PageTrackingWrapper = () => {
+  usePageTracking(); // Called within Router context
+  useEffect(() => {
+    document.title = "Sammi Web Dev";
+  }, []);
+  return null; // This component doesn't render anything
+};
 
 export default App;
