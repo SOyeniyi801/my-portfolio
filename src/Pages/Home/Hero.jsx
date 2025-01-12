dfd46be3c6dba66f63b8ec263ceefac26ed5b1f9
soyeniyi801 <naturalbeauty801@gmail.com>
2025-01-12 01:24:09 -0500

    mobile view fix

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-scroll";
 
 function Hero() {
   return (
-    <section className="min-h-screen flex flex-col justify-center px-8 bg-black" id='home'>
+    <section
+      className="min-h-screen flex flex-col justify-center px-8 bg-black"
+      id="home"
+    >
       <div className="max-w-6xl mx-auto w-full">
-      <div className="w-12 h-12 rounded-full overflow-hidden ring-2 ring-pink-500">
-  <img 
-    src="./assets/hero-image.png" 
-    alt="Avatar" 
-    className="w-full h-full object-cover"
-  />
-</div>
-        <h1 className="text-white font-raleway font-black text-6xl md:text-7xl lg:text-8xl tracking-tight leading-tight mb-8">
+        <div className="w-12 h-12 rounded-full overflow-hidden ring-2 ring-pink-500">
+          <img
+            src="./assets/hero-image.png"
+            alt="Avatar"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <h1 className="text-white font-raleway font-black text-4xl md:text-6xl lg:text-8xl tracking-tight leading-tight mb-8">
           CREATIVE DESIGNER
           <br />
           SPECIALIZING IN
           <br />
-          <span className="font-opensans text-pink-500">UI / UX DESIGN</span> 
+          <span className="font-opensans text-pink-500 text-3xl md:text-5xl lg:text-6xl">
+            UI / UX DESIGN
+          </span>
         </h1>
-        
+
         <div className="mt-12">
           <Link
             to="contact"
