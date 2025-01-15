fa198030b629bff6ee81eca3718d24ed6762b8c4
soyeniyi801 <naturalbeauty801@gmail.com>
2025-01-15 09:51:39 -0500

    add platforms

--- src/Pages/Home/Projects.jsx
+++ src/Pages/Home/Projects.jsx
@@ -21,6 +21,7 @@ function Projects() {
               />
               <div className="p-5">
                 <h3 className="text-2xl font-raleway font-bold mb-3">{project.title}</h3>
+                <p className="text-sm font-open-sans mb-5">{project.platform}</p>
                 <p className="text-sm font-open-sans mb-5">{project.description}</p>
                 <a
                   href={project.projectSite}
